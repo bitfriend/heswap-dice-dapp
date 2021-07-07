17a4c687634feded7c5e0fc79235b7a2018384ee
bitfriend <bitfriend@hotmail.com>
2021-07-07 17:37:33 +0200

    Change truffle config for testnet deployment

--- truffle-config.js
+++ truffle-config.js
@@ -18,6 +18,15 @@
  *
  */
 
+require('dotenv').config(); // To use process.env variable
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const MNEMONIC = process.env.MNEMONIC;
+const ROPSTEN_URL = `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`;
+const KOVAN_URL = `https://kovan.infura.io/v3/${process.env.INFURA_PROJECT_ID}`;
+const RINKEBY_URL = `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`;
+const GOERLI_URL = `https://goerli.infura.io/v3/${process.env.INFURA_PROJECT_ID}`;
+const MAINNET_URL = `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`;
+
 // const HDWalletProvider = require('@truffle/hdwallet-provider');
 // const infuraKey = "fj4jll3k.....";
 //
@@ -44,7 +53,7 @@ module.exports = {
     //
     development: {
       host: "127.0.0.1",     // Localhost (default: none)
-      port: 7545,            // Standard Ethereum port (default: none)
+      port: 9545,            // Truffle develop
       network_id: "*",       // Any network (default: none)
     },
     // Another network with more advanced options...
@@ -58,14 +67,27 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    ropsten: {
+      provider: () => new HDWalletProvider(MNEMONIC, ROPSTEN_URL),
+      network_id: 3,
+      gas: 300000 // ropsten has a lower block limit than mainnet
+    },
+    kovan: {
+      provider: () => new HDWalletProvider(MNEMONIC, KOVAN_URL),
+      network_id: 42
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(MNEMONIC, RINKEBY_URL),
+      network_id: 4
+    },
+    goerli: {
+      provider: () => new HDWalletProvider(MNEMONIC, GOERLI_URL),
+      network_id: 5
+    },
+    mainnet: {
+      provider: () => new HDWalletProvider(MNEMONIC, MAINNET_URL),
+      network_id: 1
+    },
     // Useful for private networks
     // private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
